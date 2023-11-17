4868da90585a17bd67059cf6a06bd3af2dcfaac6
ajla.sisic <ajla.sisic@stu.ibu.edu.ba>
2023-11-17 13:43:06 +0100

    remove unused method

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -5,13 +5,7 @@ const { browser } = require("@wdio/globals");
  * that is shared across all page objects
  */
 module.exports = class Page {
-  /**
-   * Opens a sub page of the page
-   * @param path path of the sub page (e.g. /path/to/page.html)
-   */
-  open() {
-    return browser.url('/');
-  }  
+  
   async waitForDisplayed(el) {
    await el.waitForDisplayed({ timeout: 10000 })
   }
